6a27b3ea60f6b45b5df254ceb716ab6e4d85a1d3
Marcilio Junior <marciliojrs@gmail.com>
2021-02-24 11:21:42 -0300

    Initial Commit

---
+++ HybridTesting/HybridTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HybridTestingApp.swift
+//  HybridTesting
+//
+//  Created by Marcilio Junior on 24/02/21.
+//
+
+import SwiftUI
+
+@main
+struct HybridTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
